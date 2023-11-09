ccac35b7ff6daee19f1c4ed0fedf94c993ea10eb
sanjeev <>
2023-11-09 06:58:28 -0500

    feat: integrate tina cloud

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+	branch,
+	clientId: "e0552649-e227-4fd4-9805-93955435697a",
+	token: "37c7edc846a16da993fb7d1afa42a4a37c8d7504",
+
+	build: {
+		outputFolder: "admin",
+		publicFolder: "public",
+	},
+	media: {
+		tina: {
+			mediaRoot: "",
+			publicFolder: "public",
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: "post",
+				label: "Posts",
+				path: "content/blog",
+				fields: [
+					{
+						type: "string",
+						name: "title",
+						label: "Title",
+						isTitle: true,
+						required: true,
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+					},
+				],
+			},
+		],
+	},
+});
